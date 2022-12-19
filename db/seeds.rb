9f0767a055def8b5e725573d608355a1796df7e4
Yetunde Alese <yaalese1@gmail.com>
2022-12-19 11:53:06 -0500

    commit tree 9a1f6949dec3a26e1ac19dcc1ae3c9c0d7fc5933

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,17 @@
 # Add a console message so we can see output when the seed file runs
+# Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+# Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+# Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+# Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
+puts "Seeding games..."
+# run a loop 50 times
+50.times do
+    # create a game with random data
+    Game.create(
+      title: Faker::Game.title,
+      genre: Faker::Game.genre,
+      platform: Faker::Game.platform,
+      price: rand(0..60) # random number between 0 and 60
+    )
+  end
+  puts "Done seeding!"
